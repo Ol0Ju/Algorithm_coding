e2a26f6f2a208e155192d6fb9d2f17e39a8843ee
Ol0Ju <ejwj9900@naver.com>
2025-06-24 19:08:02 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int n1=Integer.parseInt(st.nextToken());
+		int n2=Integer.parseInt(st.nextToken());
+		System.out.println(n1+n2);
+		System.out.println(n1-n2);
+		System.out.println(n1*n2);
+		System.out.println(n1/n2);
+		System.out.println(n1%n2);
+
+	}
+
+}
