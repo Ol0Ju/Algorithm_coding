7c6b3c2e9f5fdccc8b446c045a20f9cfd23a7ea2
Ol0Ju <ejwj9900@naver.com>
2025-07-01 19:10:24 +0900

    [Bronze V] Title: A+B - 8, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -3,15 +3,16 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args)throws IOException {
 		BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		//BufferedWriter w=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb=new StringBuilder();
 		int T=Integer.parseInt(r.readLine());
 		for(int i=1;i<=T;i++) {
 			String[] line=r.readLine().split(" ");
 			int A=Integer.parseInt(line[0]);
 			int B=Integer.parseInt(line[1]);
-			w.write("Case #"+i+": "+A+" + "+B+" = "+(A+B)+"\n");
+			sb.append("Case #").append(i).append(": ").append(A).append(" + ")
+			.append(B).append(" = ").append(A+B).append("\n");
 			}
-		w.flush();
-		w.close();
+		System.out.println(sb);
 	}
 }
