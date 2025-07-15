6a0484ae9fbeaf0acfd9ad57eb7c937d82c54d43
Ol0Ju <ejwj9900@naver.com>
2025-07-15 18:44:27 +0900

    [Bronze V] Title: N 찍기, Time: 816 ms, Memory: 31748 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
 		for(int i=0;i<n;i++) {
 			System.out.println(i+1);
 		}
