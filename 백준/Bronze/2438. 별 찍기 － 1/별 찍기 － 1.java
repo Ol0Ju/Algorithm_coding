afee66fb7ca3fd0670e33e1e664c3ee3728ea6a7
Ol0Ju <ejwj9900@naver.com>
2025-09-17 21:10:23 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -4,13 +4,15 @@ public class Main {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
 		
 		int N=Integer.parseInt(br.readLine());
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<i+1;j++) {
-				System.out.print("*");
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i;j++) {
+				sb.append("*");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 }
