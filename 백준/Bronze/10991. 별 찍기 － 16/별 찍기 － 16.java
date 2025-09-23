c1b7e1a656549360ca512fd5073df2232959937d
Ol0Ju <ejwj9900@naver.com>
2025-09-23 16:53:48 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main{
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		StringBuilder sb=new StringBuilder();
+		for(int i=0;i<N;i++) {
+			for(int j=N-i;j>1;j--){
+				sb.append(" ");
+			}
+			for(int j=0;j<i+1;j++) {
+				sb.append("* ");
+			}
+			sb.append("\n");
+		}
+		br.close();
+		System.out.println(sb);
+	}
+
+}
