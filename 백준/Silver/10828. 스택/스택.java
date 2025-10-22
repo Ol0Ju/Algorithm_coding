7972318ff74a0671d4fb7696d2ed29bdd3289220
강은주 <ejwj9900@naver.com>
2025-10-22 10:31:06 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 20460 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,36 +1,65 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Stack{
+	private int[] stack;
+	private int top;
 	
+	public Stack(int size) {
+		stack=new int[size];
+		top=-1;
+	}
+	
+	public void push(int value) {
+		stack[++top]=value;
+	}
+	
+	public int pop() {
+		if(top==-1)	return -1;
+		return stack[top--];
+	}
+	
+	public int size() {
+		return top+1;
+	}
+	
+	public int empty() {
+		return (top==-1)?1:0;
+	}
+	
+	public int top() {
+		if(top==-1) return -1;
+		return stack[top];
+	}
+}
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb= new StringBuilder();
+		
 		int N=Integer.parseInt(br.readLine());
-		
-		Stack<Integer> stack=new Stack<>(); //스택함수
-		
+		Stack stack=new Stack(N);
 		for(int i=0;i<N;i++) {
 			String input=br.readLine();
 			String[] cmd=input.split(" ");
-			switch (cmd[0]) {
-			case "push":
-				int value=Integer.parseInt(cmd[1]);
-				stack.push(value);
-				break;
-			case "pop":
-				sb.append(stack.isEmpty()?-1:stack.pop()).append("\n");
-				break;
-			case "size":
-				sb.append(stack.size()).append("\n");
-				break;
-			case "empty":
-				sb.append(stack.isEmpty()?1:0).append("\n");
-				break;
-			case "top":
-				sb.append(stack.isEmpty()?-1:stack.peek()).append("\n");
-				break;
-			}
+			
+			switch(cmd[0]) {
+				case "push":
+					stack.push(Integer.parseInt(cmd[1]));
+					break;
+				case "pop":
+					sb.append(stack.pop()).append("\n");
+					break;
+				case "size":
+					sb.append(stack.size()).append("\n");
+					break;
+				case "empty":
+					sb.append(stack.empty()).append("\n");
+                    break;
+				case "top":
+					sb.append(stack.top()).append("\n");
+                    break;
+			}		
 		}
 		System.out.println(sb);
 	}
