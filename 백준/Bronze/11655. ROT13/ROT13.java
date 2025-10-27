cd82ecd03636e33f205253936616e425975593b1
강은주 <ejwj9900@naver.com>
2025-10-27 10:41:43 +0900

    [Bronze I] Title: ROT13, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -7,14 +7,14 @@ public class Main {
 		String s=br.readLine();
 		for(int i=0;i<s.length();i++) {
 			char c=s.charAt(i);
-			if(c>=97&&c<=122) {
+			if(c>='a'&&c<='z') {
 				c+=13;
-				if(c>122) {
+				if(c>'z') {
 					c-=26;
 				}
-			}else if(c>=65&&c<=90) {
+			}else if(c>='A'&&c<='Z') {
 				c+=13;
-				if(c>90) {
+				if(c>'Z') {
 					c-=26;
 				}
 			}
