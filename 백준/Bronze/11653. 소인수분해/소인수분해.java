352c498be962068217395fa1d971ac8435e2086e
강은주 <ejwj9900@naver.com>
2025-11-11 16:59:46 +0900

    [Bronze I] Title: 소인수분해, Time: 112 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		for(int i=2;i<=Math.sqrt(N);i++) {
+			while(N%i==0) {
+				sb.append(i).append("\n");
+				N/=i;
+			}
+		}
+		//마지막 남은 소수 출력
+		if(N!=1) {
+			sb.append(N).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
